813579796d2fe634e7a3c1a01d02f0ed103bda49
mustang <helloMustang@json.com>
2019-09-28 18:57:40 +0800

    创建项目

---
+++ src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home"></div>
+</template>
+
+<script>
+export default {
+  name:'wraper',
+  components:{},
+  props:{},
+  data(){
+    return {
+    }
+  },
+  watch:{},
+  computed:{},
+  methods:{},
+  created(){},
+  mounted(){}
+}
+</script>
+<style lang="scss" scoped>
+.home{
+    background: red;
+}
+</style>
